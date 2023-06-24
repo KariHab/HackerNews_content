1db6b1137278a735f5d621009b859cf97037282d
KariHab <habboutk@outlook.com>
2023-06-24 13:19:34 -0400

    okay

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Hacker News Content
 
 Using the API from https://github.com/HackerNews/API
+
+
+This is a project in progress
+--------------------------------
+
+# What it does
+* Retrieve 5 last news from the website
+* Give you the specifications of each article
+* Give you the URL if you want to read it on the website directly!
+
+# How to use it
+* This project needs:
+    * requests - Package Install: pip install requests
+    * pyfiglet - Package install: pip install pyfiglet
+    * time - Package install: pip install time
+* Or run 'pip install -r requirements.txt' -> this will get you all set to read!
+* Run the bash script './read.sh' in your terminal 
+* Read & Enjoy!
--- retrieve_news.py
+++ retrieve_news.py
@@ -1,21 +1,26 @@
 import requests
 import time
-import colorama
+import pyfiglet as pf
 
-# Make an API Call and store the response
+
+print('\033[1;33;40m',pf.figlet_format('Hackar News', font = "cybermedium"),'\033[1;37;40m')
+
+
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
-r = requests.get(url)
+result = requests.get(url)
 
-# # Process Information About Each Submission
-submission_ids = r.json()
+# Process Information About Each Submission
+submission_ids = result.json()
 submission_dicts = []
 print("Fetching your articles...")
-for submission_id in submission_ids[:10]:
+
+
+for submission_id in submission_ids[:5]:
     # Make a separate api call for each submission
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
-    r = requests.get(url)
-    response_dict = r.json()
-    
+    result = requests.get(url)
+    response_dict = result.json()
+        
     # Build  a dict for each article
     submission_dict = {
         'title': response_dict['title'],
@@ -26,8 +31,9 @@ for submission_id in submission_ids[:10]:
     submission_dicts.append(submission_dict)
 
 
+
 for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dict['title']}")
+    print(f"\033[1;34;40m\nTitle: {submission_dict['title']}\033[1;37;40m")
     print(f"Author: {submission_dict['author']}")
     print(f"URL: {submission_dict['hn_link']}")
     print(f"Article Score: {submission_dict['article_score']}")
