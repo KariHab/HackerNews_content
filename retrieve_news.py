2b56cf91a51e0e30fdd110f4d132b4c848d6cb06
KariHab <habboutk@outlook.com>
2023-06-24 14:06:56 -0400

    working

--- retrieve_news.py
+++ retrieve_news.py
@@ -4,37 +4,30 @@ import pyfiglet as pf
 
 
 print('\033[1;33;40m',pf.figlet_format('Hackar News', font = "cybermedium"),'\033[1;37;40m')
-
-
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 result = requests.get(url)
-
-# Process Information About Each Submission
-submission_ids = result.json()
-submission_dicts = []
-print("Fetching your articles...")
+news_ids = result.json()
+news = []
+print("...Gathering your articles...")
 
 
-for submission_id in submission_ids[:5]:
-    # Make a separate api call for each submission
-    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+for new_id in news_ids[:5]:
+    url = f"https://hacker-news.firebaseio.com/v0/item/{new_id}.json"
     result = requests.get(url)
-    response_dict = result.json()
-        
-    # Build  a dict for each article
-    submission_dict = {
-        'title': response_dict['title'],
-        'author': response_dict['by'],
-        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
-        'article_score': response_dict['score']
+    data_collected = result.json()
+    new_info = {
+        'title': data_collected['title'],
+        'author': data_collected['by'],
+        'hn_link': f"http://news.ycombinator.com/item?id={new_id}",
+        'article_score': data_collected['score']
     }
-    submission_dicts.append(submission_dict)
+    news.append(new_info)
 
 
 
-for submission_dict in submission_dicts:
-    print(f"\033[1;34;40m\nTitle: {submission_dict['title']}\033[1;37;40m")
-    print(f"Author: {submission_dict['author']}")
-    print(f"URL: {submission_dict['hn_link']}")
-    print(f"Article Score: {submission_dict['article_score']}")
+for new_info in news:
+    print(f"\033[1;34;40m\nTitle: {new_info['title']}\033[1;37;40m")
+    print(f"Author: {new_info['author']}")
+    print(f"URL: {new_info['hn_link']}")
+    print(f"Article Score: {new_info['article_score']}")
     time.sleep(0.2)
