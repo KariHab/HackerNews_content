8cd93ca539e2a3680a7162929a18c817c58f9899
Karima Habbout <habboutk@outlook.com>
2024-04-05 18:56:13 -0400

    GUI en cours

--- retrieve_news.py
+++ retrieve_news.py
@@ -3,9 +3,7 @@
 
 
 import requests
-import time
-import pyfiglet as pf
-from tkinter import Tk, Label, Button, Text
+from tkinter import Tk, Label, Button
 import webbrowser as wb
 
 
@@ -16,16 +14,13 @@ def callback(url):
 window = Tk()
 window.configure(background='black', width=250, height=300)
 window.title("Hacker News Content")
-# print('\033[1;33;40m')
-# print('\033[1;33;40m',pf.figlet_format('Hacker News', font = "cybermedium"),'\033[1;37;40m')
 url = 'https://hacker-news.firebaseio.com/v0/beststories.json'
 result = requests.get(url)
 news_ids = result.json()
 news = []
-# print("...Gathering your articles...")
 
 
-for new_id in news_ids[:3]:
+for new_id in news_ids[:8]:
     url = f"https://hacker-news.firebaseio.com/v0/item/{new_id}.json"
     result = requests.get(url)
     data_collected = result.json()
@@ -38,16 +33,12 @@ for new_id in news_ids[:3]:
     }
     news.append(new_info)
 
-for new_info in news:
-    Label(window, text="Title: " + new_info['title'], foreground="white", background="black").grid()
-    print(f"\033[1;34;40m\nTitle: {new_info['title']}\033[1;37;40m")
-    Label(window, text="Author: " + new_info['author'], foreground="white", background="black").grid()
-    Label(window, text="Link: " + new_info['url'], foreground="white", background="black").grid()
-    Button(window, text="Open the url", command=lambda url=new_info['url']: callback(url)).grid()
-    # print(f"Author: {new_info['author']}")
-    # print(f"URL: {new_info['hn_link']}")
-    # # print(f"Article Score: {new_info['article_score']}")
-    # # time.sleep(0.2)
+for index, new_info in enumerate(news):
+    Label(window, text="Title: " + new_info['title'], foreground="white", background="black").grid(row=index, sticky="w")
+
+    Label(window, text="Author: " + new_info['author'], foreground="white", background="black").grid(row=index, sticky="w")
+    # Label(window, text="Link: " + new_info['url'], foreground="white", background="black").grid(sticky="e")
+    Button(window, text="Open the url", command=lambda url=new_info['url']: callback(url)).grid(row=index,column=2, sticky="e")
 
 
 Button(window, text="close", command=window.quit).grid()
