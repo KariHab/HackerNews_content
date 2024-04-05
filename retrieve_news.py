b92c9e3ca7214b081f459b26951c8f58c9f03035
Karima Habbout <habboutk@outlook.com>
2024-04-05 18:37:39 -0400

    GUI en cours

--- retrieve_news.py
+++ retrieve_news.py
@@ -1,37 +1,54 @@
 # A simple content aggregator from  Hacker News with the use of it's API
-# Karima H - June 2023
+# Karima H - June 2023 - Updated April 2024
 
 
 import requests
 import time
 import pyfiglet as pf
+from tkinter import Tk, Label, Button, Text
+import webbrowser as wb
 
-print('\033[1;33;40m')
-print('\033[1;33;40m',pf.figlet_format('Hackar News', font = "cybermedium"),'\033[1;37;40m')
-url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+
+def callback(url):
+    wb.open_new_tab(url)
+
+
+window = Tk()
+window.configure(background='black', width=250, height=300)
+window.title("Hacker News Content")
+# print('\033[1;33;40m')
+# print('\033[1;33;40m',pf.figlet_format('Hacker News', font = "cybermedium"),'\033[1;37;40m')
+url = 'https://hacker-news.firebaseio.com/v0/beststories.json'
 result = requests.get(url)
 news_ids = result.json()
 news = []
-print("...Gathering your articles...")
+# print("...Gathering your articles...")
 
 
-for new_id in news_ids[:5]:
+for new_id in news_ids[:3]:
     url = f"https://hacker-news.firebaseio.com/v0/item/{new_id}.json"
     result = requests.get(url)
     data_collected = result.json()
     new_info = {
         'title': data_collected['title'],
         'author': data_collected['by'],
+        'url': data_collected['url'],
         'hn_link': f"http://news.ycombinator.com/item?id={new_id}",
         'article_score': data_collected['score']
     }
     news.append(new_info)
 
-
-
 for new_info in news:
+    Label(window, text="Title: " + new_info['title'], foreground="white", background="black").grid()
     print(f"\033[1;34;40m\nTitle: {new_info['title']}\033[1;37;40m")
-    print(f"Author: {new_info['author']}")
-    print(f"URL: {new_info['hn_link']}")
-    print(f"Article Score: {new_info['article_score']}")
-    time.sleep(0.2)
+    Label(window, text="Author: " + new_info['author'], foreground="white", background="black").grid()
+    Label(window, text="Link: " + new_info['url'], foreground="white", background="black").grid()
+    Button(window, text="Open the url", command=lambda url=new_info['url']: callback(url)).grid()
+    # print(f"Author: {new_info['author']}")
+    # print(f"URL: {new_info['hn_link']}")
+    # # print(f"Article Score: {new_info['article_score']}")
+    # # time.sleep(0.2)
+
+
+Button(window, text="close", command=window.quit).grid()
+window.mainloop()
