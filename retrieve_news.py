7bbfb9de8135e361a24d4aa1d47d7cc580d5e390
Karima Habbout <habboutk@outlook.com>
2024-04-09 21:24:33 -0400

    GUI updated

--- retrieve_news.py
+++ retrieve_news.py
@@ -1,9 +1,9 @@
 # A simple content aggregator from  Hacker News with the use of it's API
 # Karima H - June 2023 - Updated April 2024
-
+from tkinter.font import Font
 
 import requests
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, Frame
 import webbrowser as wb
 
 
@@ -12,9 +12,13 @@ def callback(url):
 
 
 window = Tk()
-# window.geometry("500x600")
-window.configure(background='black')
+window.resizable(False, False)
+window.configure(background='#4E51BA')
 window.title("Hacker News Content")
+bold_font = Font(family="Arial", size=15, weight="bold")
+Label(window, text="     ", background="#7B7DB5").grid(columnspan=4, sticky='nsew')
+Label(window, text="Hacker News Content", foreground="black", background="#7B7DB5", font=bold_font).grid(columnspan=4, sticky='nsew')
+
 url = 'https://hacker-news.firebaseio.com/v0/beststories.json'
 result = requests.get(url)
 news_ids = result.json()
@@ -33,13 +37,14 @@ for new_id in news_ids[:8]:
         'article_score': data_collected['score']
     }
     news.append(new_info)
+Label(window, text="Top 8 Best Stories", background="#7B7DB5", foreground='black', font="Arial, 15").grid(columnspan=4, sticky='nsew')
+Label(window, text="     ", background="#7B7DB5").grid(columnspan=4, sticky='nsew')
+
 
 for index, new_info in enumerate(news):
-    Label(window, text="Title: " + new_info['title'], foreground="white", background="black").grid(row=index, column=0, sticky="w")
-    Label(window, text="Author: " + new_info['author'], foreground="white", background="black").grid(row=index,column=1, sticky="w")
-    Label(window, text="Article score: " +str(new_info['article_score']), foreground="white", background="black").grid(row=index,column=2,sticky="w")
-    Button(window, text="Open the url", command=lambda url=new_info['url']: callback(url)).grid(row=index, column=3,sticky="e")
+    Label(window, text=new_info['title'], foreground="white", background='#4E51BA', font=('Arial',11, 'bold')).grid(row=index+4, column=0, sticky='w')
+    Label(window, text="by " + new_info['author'], foreground="white", background='#4E51BA', font='Arial, 9').grid(row=index+4, column=1, sticky='w')
+    Label(window, text="score: " + str(new_info['article_score']), foreground="white", background='#4E51BA', font='Arial, 9').grid(row=index+4, column=2, sticky='w')
+    Button(window, text="Read the full article", command=lambda url=new_info['url']: callback(url), font=('Arial', 9, 'bold'), background="#C3C8EE").grid(row=index+4, column=3,sticky='e')
 
-#
-# Button(window, text="close", command=window.quit).grid()
 window.mainloop()
